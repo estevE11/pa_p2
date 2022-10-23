c4bfb57dabbf85690674dce482844620634441a1
Roger Esteve <rogeresteve30@gmail.com>
2022-10-23 13:55:46 +0200

    header poblacio

---
+++ Poblacio.h
@@ -0,0 +1,30 @@
+#include <ContenidorBrossa.h>
+
+#include <string>
+
+class Poblacio {
+private:
+    struct node {
+        ContenidorBrossa * con;
+        node* seg;
+    };
+
+    /**
+     * Array per emmagatzemar els nodes capçelera de les llistes enllaçades
+     */
+    node **contenidor;
+
+public:
+    Poblacio();
+    Poblacio(ContenidorBrossa* g);
+    ~Poblacio();
+    void afegirContenidor(ContenidorBrossa* g);
+    void afegirContenidor(std::string codi, int color, std::string ubicacio, int anyColocacio, float tara);
+    // Se li passa un codi i ha de retornar el color del contenidor, sino existeix llança una exepcio
+    std::string hiEs(std::string codi);
+    void eliminarContenidor(ContenidorBrossa *c);
+    ContenidorBrossa *mesRendiment();
+    int getQuants(int color);
+    int getQuants();
+    void toString();
+};
