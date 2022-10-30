281c0b1df27e60d2a3bf5d18e14d6b1e85e6a9c6
Roger Esteve <rogeresteve30@gmail.com>
2022-10-30 18:35:04 +0100

    metode afegirContenidor

--- Poblacio.cpp
+++ Poblacio.cpp
@@ -1,7 +1,12 @@
 #include "Poblacio.h"
 
+#include <iostream>
+
 Poblacio::Poblacio() {
     this->contenidors = new node*[5];
+    for(int i = 0; i < 5; i++) {
+        this->contenidors[i] = nullptr;
+    }
 }
 
 Poblacio::Poblacio(ContenidorBrossa* g) : Poblacio() {
@@ -13,7 +18,35 @@ Poblacio::~Poblacio() {
 }
 
 void Poblacio::eliminarContenidor(ContenidorBrossa* c) {
-    
+
+}
+
+void Poblacio::afegirContenidor(ContenidorBrossa* p) {
+    std::string t = p->getType();
+    int id = 0;
+    if(t == "plastic") id = 0;
+    else if(t == "organic") id = 1;
+    else if(t == "rebuig") id = 2;
+    else if(t == "vidre") id = 3;
+    else if(t == "paper") id = 4;
+
+    node* curr = this->contenidors[id];
+    if(curr == nullptr) {
+        node* n = new node;
+        n->con = p;
+        n->seg = nullptr;
+        this->contenidors[id] = n;
+        return;
+    }
+    if(curr->con == p) throw("El contenidor ja esxisteix!");
+    while(curr->seg) {
+        curr = curr->seg;
+        if(curr->con == p) throw("El contenidor ja esxisteix!");
+    }
+
+    node* n = new node;
+    n->con = p;
+    curr->seg = n;
 }
 
 void Poblacio::afegirContenidor(std::string codi, int color, std::string ubicacio, int anyColocacio, float tara) {
@@ -34,4 +67,12 @@ int Poblacio::getQuants() {
 
 void Poblacio::toString() {
     
+}
+
+ContenidorBrossa *Poblacio::mesRendiment() {
+    return nullptr;
+}
+
+int Poblacio::getQuants(int color) {
+    return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 
 #include "Organic.h"
+#include "Poblacio.h"
 
 int main() {
     Organic organic("XY-000",  "follandome a tu madre", 2022, 12);
     organic.buidat(14);
 
-    Organic test("XY-001", 12);
+    Organic organic2("XY-001",  "follandome a tu madre", 2022, 12);
+    organic.buidat(200);
 
-    organic.toString();
-    std::cout << "----------" << std::endl;
-    test.toString();
+
+    Poblacio p;
+    p.afegirContenidor(&organic);
+    p.afegirContenidor(&organic2);
+    p.afegirContenidor(&organic2);
 
     return 0;
 }
